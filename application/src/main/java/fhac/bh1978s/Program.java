63358e7fe9d7236a93ccab0438a8c4146dd581ca
Bjarne Herrmann <bjarne_herrmann@gothaer-systems.de>
2020-04-28 16:00:12 +0200

    Finish pom config: javadoc, assembly, packaging

---
+++ application/src/main/java/fhac/bh1978s/Program.java
@@ -0,0 +1,15 @@
+package fhac.bh1978s;
+
+public class Program {
+
+
+  /**
+   * Haupteinstiegspunkt des Programms
+   * @param args
+   * Argumente für den Pfad des Ordners zum Lesen der Eingabedateien sowie
+   * Pfad des Ordners für die Generierung der Ausgabedateien.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hallo NEUE Welt!");
+  }
+}
