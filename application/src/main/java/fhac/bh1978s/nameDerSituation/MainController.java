350321b1b4e02a1da62b55065081554c9e3f5575
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2020-06-04 13:47:50 +0200

    Add some JavaDocs-Comments

--- application/src/main/java/fhac/bh1978s/nameDerSituation/MainController.java
+++ application/src/main/java/fhac/bh1978s/nameDerSituation/MainController.java
@@ -1,20 +1,24 @@
 package fhac.bh1978s.nameDerSituation;
 
+import fhac.bh1978s.ioStream.IOFileWriter;
 import fhac.bh1978s.ioStream.IOTextFileReader;
 import fhac.bh1978s.ioStream.IOFileReader;
+import fhac.bh1978s.ioStream.IOTextFileWriter;
 import fhac.bh1978s.nameDerSituation.mapper.GenericMapper;
 
 /**
- * MainController für die Verwaltung der Aufgabe. Dieser ist im Singleton-Pattern-Style implementiert.
+ * Haupt-Verarbeitungscontroller vom MVC-Pattern als Singleton-Pattern-Style implementiert.
+ * Dieser dient als Verarbeitungsschnittstelle zwischen der View (Eingabe/Ausgabe) und den Models.
  */
 public class MainController {
 
   private static MainController mainController = new MainController();
-  private IOFileReader ioFileReader = IOTextFileReader.getInstance();
+
+  private IOFileReader<String> ioFileReader = IOTextFileReader.getInstance();
+  private IOFileWriter<String> ioFileWriter = IOTextFileWriter.getInstance();
+
   // TODO: Hier später mapper initialisieren
   private GenericMapper<String, Object> mapper;
-
-
 
   public static MainController getInstance() {
     return mainController;
@@ -26,20 +30,26 @@ public class MainController {
     return mainController;
   }
 
+  /**
+   * Start-Methode des Haupt-Verarbeitungscontrollers. Diese dient als Verarbeitungsschnittstelle zwischen
+   * dem Lesen/Schreiben von Dateien, dem Mappen von Datenstrukturen, als auch der Deligierung an weitere Controller.
+   */
   public void start() {
+    try {
+      // 1. Lese Dateien --> erhalte List<String> (pro Item ein Dateiinhalt - bei Probleme: Fehlercode in Liste)
 
-    // 1. Lese Dateien --> erhalte List<String> (pro Item ein Dateiinhalt - bei Probleme: Fehlercode in Liste)
+      // 2. Mappe List<String> in interne Datenobjekte (ignoriere Fehler, diese werden später in output geschrieben)
 
-    // 2. Mappe List<String> in interne Datenobjekte (ignoriere Fehler, diese werden später in output geschrieben)
+      // 3. Führe Aufgabe für jede Datei (jedes interne Datenobjekt) aus und erhalte Liste mit Lösungsobjekten
 
-    // 3. Führe Aufgabe für jede Datei (jedes interne Datenobjekt) aus und erhalte Liste mit Lösungsobjekten
+      // 4. Mappe List<Lösungsobjekte> in externe List<String> für Ausgabe (beachte vorher nicht berechnete Objekte aufgrund von Fehlern)
 
-    // 4. Mappe List<Lösungsobjekte> in externe List<String> für Ausgabe (beachte vorher nicht berechnete Objekte aufgrund von Fehlern)
+      // 5. Generiere Output-Dateien aus List<String>
 
-    // 5. Generiere Output-Dateien aus List<String>
+      // Sonstiges: Bei unerwarteten Fehlern Log-Datei in _devLogs erstellen für Entwickler
+    } catch (Exception e) {
 
-    // Sonstiges: Bei unerwarteten Fehlern Log-Datei in _devLogs erstellen für Entwickler
-
+    }
     System.out.println("Szeraio start...");
   }
 }
