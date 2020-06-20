d4f911b29a2c932a179fcc1cf7570056ebaa7ac5
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2020-06-20 16:10:22 +0200

    Edit mapper-comments

--- application/src/main/java/fhac/bh1978s/nameDerSituation/mapper/I_InputMapper.java
+++ application/src/main/java/fhac/bh1978s/nameDerSituation/mapper/I_InputMapper.java
@@ -1,9 +1,10 @@
 package fhac.bh1978s.nameDerSituation.mapper;
 
 /**
- * Generische Mapper-Klasse, welche Methoden für das Mappen vom externen Dateiformat ins interne Datenformat
- * sowie umgekehrt implementiert.
+ * Generische Mapper-Klasse, welche Methoden für das Mappen vom externen Dateiformat ins interne
+ * Datenformat implementiert. E definiert das Objekt des externen Formats, I des internen.
  */
 public interface I_InputMapper<E, I> {
+
   I mapToInternFormat(E externContent);
 }
--- application/src/main/java/fhac/bh1978s/nameDerSituation/mapper/I_OutputMapper.java
+++ application/src/main/java/fhac/bh1978s/nameDerSituation/mapper/I_OutputMapper.java
@@ -1,6 +1,14 @@
 package fhac.bh1978s.nameDerSituation.mapper;
 
+/**
+ * Generische Mapper-Klasse, welche Methoden für das Mappen vom internen Dateiformat ins externe
+ * Datenformat implementiert. E definiert das Objekt des externen Formats, I_Valid ein valides
+ * Objekt des berechneten Ergebnisses und I_Error das von der Eingabe konvertierte Objekt mit
+ * Semantikfehlern.
+ */
 public interface I_OutputMapper<I_Valid, I_Error, E> {
+
   E mapToExternFormat(I_Valid internContent);
+
   E mapErrorToExternFormat(I_Error internContent);
 }
